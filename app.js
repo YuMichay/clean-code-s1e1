13edf28b1fa8f4d2041c12232668a8f7fe6e68c3
yuliya <julia.one.such@gmail.com>
2022-05-30 11:15:22 +0300

    fix: rename classes according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,22 +8,21 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var taskInput=document.querySelector(".form__add_new");//Add a new task.
+var addButton=document.querySelector(".form__button");//first button
+var incompleteTaskHolder=document.querySelector(".form__incompleted");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".form__completed");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.classList.add("item");
+    listItem.classList.add("form__item");
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
-    checkBox.classList.add("input");
-    checkBox.classList.add("input-checkbox");
+    checkBox.classList.add("form__checkbox");
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -35,24 +34,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
-    label.classList.add("label");
+    label.className='form__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
-    editInput.classList.add("input");
-    editInput.classList.add("input-text");
+    editInput.className="form__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
-    editButton.classList.add("button");
+    editButton.className="form__button_edit";
+    editButton.classList.add("form__button");
 
-    deleteButton.className="delete";
-    deleteButton.classList.add("button");
+    deleteButton.className="form__button_delete";
+    deleteButton.classList.add("form__button");
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.classList.add("delete-image");
+    deleteButtonImg.classList.add("form__image");
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -90,10 +86,10 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('.input-text');
+    var editInput=listItem.querySelector('.form__input');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var editBtn=listItem.querySelector(".form__button_edit");
+    var containsClass=listItem.classList.contains("form__item_edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -107,7 +103,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("form__item_edit");
 };
 
 
@@ -163,9 +159,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector(".input-checkbox");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".form__checkbox");
+    var editButton=taskListItem.querySelector(".form__button_edit");
+    var deleteButton=taskListItem.querySelector(".form__button_delete");
 
 
     //Bind editTask to edit button.
